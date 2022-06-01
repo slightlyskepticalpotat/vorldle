3d1cdb3b5d603ea98d88ffeef7b9a9de9cf50e0c
Tony Zhang <76820191+Tonythezhanger@users.noreply.github.com>
2022-06-01 10:10:58 -0400

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -1,1 +1,18 @@
+static GraphicsDevice device = GraphicsEnvironment
+        .getLocalGraphicsEnvironment().getScreenDevices()[0];
 
+public static void main(String[] args) {
+
+    final JFrame frame = new JFrame("Worldle");
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.setUndecorated(true);
+
+    device.setFullScreenWindow(frame);
+
+    JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+    frame.add(panel);
+
+    frame.pack();
+    frame.setVisible(true);
+
+}

10bac2280d8ab1f032edbc84c481424921e252c3
Anthony Chen <chenanthony365@gmail.com>
2022-06-06 09:15:21 -0400

    gamepanel placeholder code

---
+++ GamePanel.java
@@ -0,0 +1,48 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GamePanel extends JPanel implements Runnable {
+    public Thread gameThread;
+    public Image image;
+    public Graphics graphics;
+
+    public GamePanel() {
+        this.setFocusable(true);
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void paint(Graphics g) {
+        image = createImage(512, 512);
+        graphics = image.getGraphics();
+        draw(graphics);
+        g.drawImage(image, 0, 0, this);
+    }
+
+    public void draw(Graphics g) {
+        DrawArrow.draw();
+        Countries.draw();
+    }
+
+    public void run(){
+        // the CPU runs our game code too quickly - we need to slow it down! The following lines of code "force" the computer to get stuck in a loop for short intervals between calling other methods to update the screen.
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60;
+        double ns = 1000000000/amountOfTicks;
+        double delta = 0;
+        long now;
+
+        while(true){ // this is the infinite game loop
+            now = System.nanoTime();
+            delta = delta + (now-lastTime)/ns;
+            lastTime = now;
+
+            // only move objects around and update screen if enough time has passed
+            if(delta >= 1){
+                repaint();
+                delta--;
+            }
+        }
+    }
+}

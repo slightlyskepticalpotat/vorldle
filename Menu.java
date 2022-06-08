5ba76c85d1b0299c22f0a540f196e9c47f2cd0bd
Anthony Chen <chenanthony365@gmail.com>
2022-06-08 09:20:13 -0400

    move text

--- Menu.java
+++ Menu.java
@@ -1,21 +1,21 @@
 import java.awt.*;
 
 public class Menu extends Rectangle{
-	
-	public static int GAME_WIDTH;
-	public static int GAME_HEIGHT;
-	
-	public Menu(int w, int h) {
-		Menu.GAME_WIDTH = w;
-		Menu.GAME_HEIGHT = h;
+
+	public static int X_POS;
+	public static int Y_POS;
+
+	public Menu(int x, int y) {
+		Menu.X_POS = x;
+		Menu.Y_POS = y;
 	}
-	
+
 	public void draw(Graphics g) {
 		Color title = new Color(48,163,84);
-		
+
 		g.setColor(title);
 		g.setFont(new Font("Segoe UI", Font.BOLD, 30));
-		g.drawString(String.valueOf("VORLDLE"), (int)(GAME_WIDTH*0.4),(int)(GAME_HEIGHT*0.2));
+		g.drawString(String.valueOf("VORLDLE"), Menu.X_POS,Menu.Y_POS);
 	}
-	
+
 }

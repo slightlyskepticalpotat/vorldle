cdca61c7d1ed2a598f39df872b47cfe518946903
Tony Zhang <76820191+Tonythezhanger@users.noreply.github.com>
2022-06-10 09:12:18 -0400

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -1,68 +1,117 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 public class GamePanel extends JPanel implements Runnable {
-    public Thread gameThread;
-    public Image image;
-    public Graphics graphics;
-    public Countries countries;
-    public int needsReset = 1;
-    public String currentCountry;
-    public Menu menu;
+	public Thread gameThread;
+	public Image image;
+	public Graphics graphics;
+	public Countries countries;
+	public int needsReset = 1;
+	public String currentCountry;
+	public Menu menu;
 
-    public GamePanel() {
-        this.setFocusable(true);
-        gameThread = new Thread(this);
-        gameThread.start();
-        countries = new Countries("database.txt");
-        menu = new Menu(175, 50);
-        this.setPreferredSize(new Dimension(512, 512));
-    }
+	public GamePanel() {
+		this.setFocusable(true);
+		gameThread = new Thread(this);
+		gameThread.start();
+		countries = new Countries("src/database.txt");
+		menu = new Menu(190, 50);
+		MenuButton();
+		this.setPreferredSize(new Dimension(512, 768));
 
-    public void paint(Graphics g) {
-        image = createImage(512, 512);
-        graphics = image.getGraphics();
-        draw(graphics);
-        g.drawImage(image, 0, 0, this);
-    }
+	}
 
-    public void draw(Graphics g) {
-        // DrawArrow.draw();
-        // Countries.draw();
-        if (needsReset == 1) {
-            currentCountry = countries.getRandomCountry();
-            needsReset = 0;
-        }
-        try {
-            BufferedImage image = ImageIO.read(new File("img/" + currentCountry + ".png"));
-            g.drawImage(image, 0, 0, null);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        menu.draw(g);
-    }
+	public void MenuButton() {
 
-    public void run(){
-        // the CPU runs our game code too quickly - we need to slow it down! The following lines of code "force" the computer to get stuck in a loop for short intervals between calling other methods to update the screen.
-        long lastTime = System.nanoTime();
-        double amountOfTicks = 60;
-        double ns = 1000000000/amountOfTicks;
-        double delta = 0;
-        long now;
+		Icon Questionmark = new ImageIcon("Questionmark.png");
+		Icon Upwardtrend = new ImageIcon("Upwardtrend.png");
+		Icon Gear = new ImageIcon("Gear.png");
 
-        while(true){ // this is the infinite game loop
-            now = System.nanoTime();
-            delta = delta + (now-lastTime)/ns;
-            lastTime = now;
+		JButton help = new JButton(Questionmark);
+		JButton stats = new JButton(Upwardtrend);
+		JButton settings = new JButton(Gear);
 
-            // only move objects around and update screen if enough time has passed
-            if(delta >= 1){
-                repaint();
-                delta--;
-            }
-        }
-    }
+		help.setBounds(0, 0, 30, 30);
+		stats.setBounds(0, 400, 30, 30);
+		settings.setBounds(0, 450, 30, 30);
+
+		this.add(help);
+		this.add(stats);
+		this.add(settings);
+
+		help.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// Actions
+			}
+		});
+
+		stats.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// Actions
+			}
+		});
+
+		settings.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// Actions
+			}
+		});
+
+	}
+
+	public void paint(Graphics g) {
+
+		image = createImage(512, 512);
+		graphics = image.getGraphics();
+		draw(graphics);
+		g.drawImage(image, 0, 0, this);
+	}
+
+	public void draw(Graphics g) {
+		// DrawArrow.draw();
+		// Countries.draw();
+		if (needsReset == 1) {
+			currentCountry = countries.getRandomCountry();
+			needsReset = 0;
+		}
+		try {
+			BufferedImage image = ImageIO.read(getClass().getResource("/img/" + currentCountry + ".png"));
+			g.drawImage(image, 128, 128, null);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		menu.draw(g);
+	}
+
+	public void run() {
+		// the CPU runs our game code too quickly - we need to slow it down! The
+		// following lines of code "force" the computer to get stuck in a loop for short
+		// intervals between calling other methods to update the screen.
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long now;
+
+		while (true) { // this is the infinite game loop
+			now = System.nanoTime();
+			delta = delta + (now - lastTime) / ns;
+			lastTime = now;
+
+			// only move objects around and update screen if enough time has passed
+			if (delta >= 1) {
+				repaint();
+				delta--;
+			}
+		}
+	}
 }

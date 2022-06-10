363086d867f3fcc12bb1546c01616ae778018b87
Tony Zhang <76820191+Tonythezhanger@users.noreply.github.com>
2022-06-10 09:14:17 -0400

    Update Distance.java

--- Distance.java
+++ Distance.java
@@ -8,7 +8,7 @@ public class Distance {
 		double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) + Math.cos(Math.toRadians(lat1))
 				* Math.cos(Math.toRadians(lat2)) * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
 		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-		double distance = R * c * 1000; // convert to meters
+		double distance = R * c;
 
 		double height = el1 - el2;
 

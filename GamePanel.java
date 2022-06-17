89e153812c2f786fe6cbbcd9cbf3d8cc9be2e98d
Anthony Chen <31290848+slightlyskepticalpotat@users.noreply.github.com>
2022-06-17 13:19:31 -0400

    finish gui

--- GamePanel.java
+++ GamePanel.java
@@ -3,10 +3,11 @@ import java.awt.*;
 import javax.swing.*;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.awt.event.*;
 
 import java.util.Scanner;
 
-public class GamePanel extends JPanel implements Runnable {
+public class GamePanel extends JPanel implements Runnable, KeyListener {
 	public Thread gameThread;
 	public Image image;
 	public Graphics graphics;
@@ -15,16 +16,19 @@ public class GamePanel extends JPanel implements Runnable {
 	public String currentCountry;
 	public Menu menu;
 	public Proximity squares;
-	public String guess;
+	public String guess = "";
 	public Scanner scan = new Scanner(System.in);
 	public int score = 0;
 	public int guessesLeft = 6;
 	public Proximity squareCalculator;
 	public double percent;
 	public int squareLocation = 0;
+	public String textBox = "";
+	public int readyCheck = 0;
 
 	public GamePanel() {
 		this.setFocusable(true);
+		this.addKeyListener(this);
 		gameThread = new Thread(this);
 		gameThread.start();
 		countries = new Countries("src/database.txt");
@@ -45,29 +49,37 @@ public class GamePanel extends JPanel implements Runnable {
 		// DrawArrow.draw();
 		// Countries.draw();
 		if (needsReset == 0 && guessesLeft > 0) {
-			System.out.print("Guess: ");
-			guess = scan.nextLine();
-			guessesLeft -= 1;
+			if (readyCheck == 1) {
+				guess = textBox;
+				textBox = "";
+				guessesLeft -= 1;
+				readyCheck = 0;
+				guess = countries.getCode(guess);
+			}
+
+			g.setColor(Color.black);	
+			g.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+			FontMetrics metrics = g.getFontMetrics();
+			if ((System.currentTimeMillis() / 1000) % 2 == 1) {
+				g.drawString("  " + textBox, (512 - metrics.stringWidth("  " + textBox)) / 2, 700);
+			} else {
+				g.drawString("> " + textBox, (512 - metrics.stringWidth("> " + textBox)) / 2, 700);
+			}
+
 			// we check win conditions here
-			if (guess.equals(countries.getName(currentCountry))) {
+			if (countries.getName(guess).equals(countries.getName(currentCountry))) {
 				score += 1;
 				needsReset = 1;
-			} else {
-				guess = countries.getCode(guess);
+			} else if (guessesLeft != 6) {
 				if (guess.equals("No Country Found")) {
 					g.setColor(Color.gray);	
 					g.setFont(new Font("Segoe UI", Font.PLAIN, 20));
-					FontMetrics metrics = g.getFontMetrics();
+					metrics = g.getFontMetrics();
 					g.drawString("Invalid Country", (512 - metrics.stringWidth("Invalid Country")) / 2, 550);
 					g.drawString(guessesLeft + " Guesses Left", (512 - metrics.stringWidth(guessesLeft + " Guesses Left")) / 2, 600);
 				} else {
 					int distance = findDistance(countries.getLat(guess), countries.getLon(guess), countries.getLat(currentCountry), countries.getLon(currentCountry), 0, 0);
-					System.out
-							.println(
-									"The country is in the "
-											+ getBearing(countries.getLat(guess), countries.getLon(guess),
-													countries.getLat(currentCountry), countries.getLon(currentCountry))
-											+ " direction.");
+					// System.out.println("The country is in the "+ getBearing(countries.getLat(guess), countries.getLon(guess),countries.getLat(currentCountry), countries.getLon(currentCountry))+ " direction.");
 					percent = squareCalculator.PercentProximity(findDistance(countries.getLat(guess), countries.getLon(guess), countries.getLat(currentCountry), countries.getLon(currentCountry), 0, 0));
 					squareCalculator.SquareProgress(percent);
 					
@@ -89,15 +101,15 @@ public class GamePanel extends JPanel implements Runnable {
 					}
 
 					g.setColor(Color.gray);	
-					g.setFont(new Font("Segoe UI", Font.PLAIN, 32));
-					FontMetrics metrics = g.getFontMetrics();
+					g.setFont(new Font("Segoe UI", Font.PLAIN, 20));
+					metrics = g.getFontMetrics();
 					g.drawString("Guess: " + countries.getName(guess), (512 - metrics.stringWidth("Guess: " + countries.getName(guess))) / 2, 550);	
 					String message = distance + " km (" + (int) percent + "%)";
 					g.drawString(message, (512 - metrics.stringWidth(message)) / 2, 600);
 					g.drawString(guessesLeft + " Guesses Left", (512 - metrics.stringWidth(guessesLeft + " Guesses Left")) / 2, 650);
-					g.drawString("Score: " + score, (512 - metrics.stringWidth("Score: " + score)) / 2, 700);
 				}
 			}
+			g.drawString("Score: " + score, (512 - metrics.stringWidth("Score: " + score)) / 2, 850);
 		}
 		if (needsReset == 1 || guessesLeft == 0) {
 			currentCountry = countries.getRandomCountry();
@@ -106,9 +118,9 @@ public class GamePanel extends JPanel implements Runnable {
 			guessesLeft = 6;
 		}
 		g.setColor(Color.black);
-		g.setFont(new Font("Segoe UI", Font.PLAIN, 32));
+		g.setFont(new Font("Segoe UI", Font.PLAIN, 20));
 		FontMetrics metrics = g.getFontMetrics();
-		g.drawString(countries.getName(currentCountry).replaceAll("[a-zA-Z-]", "_ ").trim(), (512 - metrics.stringWidth(countries.getName(currentCountry).replaceAll("[a-zA-Z-]", "_ ").trim())) / 2, 128);
+		g.drawString(countries.getName(currentCountry).replaceAll("[a-zA-Z]", "_ ").trim(), (512 - metrics.stringWidth(countries.getName(currentCountry).replaceAll("[a-zA-Z]", "_ ").trim())) / 2, 128);
 
 		try {
 			BufferedImage image = ImageIO.read(getClass().getResource("/img/" + currentCountry + ".png"));
@@ -180,4 +192,29 @@ public class GamePanel extends JPanel implements Runnable {
 		}
 		return bearingdeg;
 	}
+
+	public void keyPressed(KeyEvent e){
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			readyCheck = 1;
+			return;
+		} else if (e.getKeyChar() == KeyEvent.VK_BACK_SPACE && textBox.length() > 0) {
+			textBox = textBox.substring(0, textBox.length() - 1); 
+		} else if (e.getKeyChar() == KeyEvent.VK_DELETE) {
+			System.exit(0);
+		}
+		if (isPrintableChar(e.getKeyChar())) {
+			textBox = textBox + e.getKeyChar();
+		}
+  	}
+
+	public void keyTyped(KeyEvent e){}
+	public void keyReleased(KeyEvent e){}
+
+	public boolean isPrintableChar( char c ) {
+        Character.UnicodeBlock block = Character.UnicodeBlock.of( c );
+        return (!Character.isISOControl(c)) &&
+                c != KeyEvent.CHAR_UNDEFINED &&
+                block != null &&
+                block != Character.UnicodeBlock.SPECIALS;
+    }
 }
--- README.txt
+++ README.txt
@@ -1,3 +1,5 @@
 # ics-project
 
 You will be given 6 chances to guess the country, given the outline and number of letters. Enter your guesses in the text box. Good luck!
+
+Pressing delete quits the program.

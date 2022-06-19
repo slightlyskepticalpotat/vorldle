6ae0a4057ad660feb17eed6f2acca35f29ef3aab
Tony Zhang <76820191+Tonythezhanger@users.noreply.github.com>
2022-06-19 02:39:55 -0400

    Update GamePanel.java

--- GamePanel.java
+++ GamePanel.java
@@ -1,4 +1,13 @@
 import java.awt.*;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
 
 import javax.swing.*;
 import javax.imageio.ImageIO;
@@ -25,6 +34,7 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 	public int squareLocation = 0;
 	public String textBox = "";
 	public int readyCheck = 0;
+	public Bearing direction;
 
 	public GamePanel() {
 		this.setFocusable(true);
@@ -128,6 +138,9 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 		} catch (Exception e) {
 			System.out.println(e);
 		}
+		
+		paintComponent(g, getBearing(countries.getLat(guess),countries.getLon(guess),countries.getLat(currentCountry), countries.getLon(currentCountry)), 1);
+		
 		menu.draw(g);
 	}
 
@@ -192,6 +205,33 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
 		}
 		return bearingdeg;
 	}
+	
+	public void paintComponent(Graphics g, double degree, double scaling) {
+		BufferedImage Arrow = LoadImage("src/Arrow.png"); // Idk import an image lol
+
+		AffineTransform at = AffineTransform.getTranslateInstance(200, 600); // change the position if needed
+		at.rotate(Math.toRadians(degree), Arrow.getWidth() / 2, Arrow.getHeight() / 2);
+		at.scale(scaling, scaling);
+
+		Graphics2D g2d = (Graphics2D) g;
+
+		g2d.drawImage(Arrow, at, null);
+		repaint();
+	}
+
+	BufferedImage LoadImage(String FileName) {
+		BufferedImage img = null;
+
+		try {
+			img = ImageIO.read(new File(FileName));
+		} catch (IOException e) {
+
+		}
+
+		return img;
+	}
+	
+	//Draws the arrow for which the country is to the direction of
 
 	public void keyPressed(KeyEvent e){
 		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
